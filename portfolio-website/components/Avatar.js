b71a1849fb406d3e80920aaed6404fa3b596be1d
Lucas Lacerda <lribeirolacerda@gmail.com>
2024-10-02 17:57:28 -0300

    Portifolio Lucas Terminar

--- portfolio-website/components/Avatar.js
+++ portfolio-website/components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-        src={"/Designer (1).png"}
+        src={"/Lucas.png"}
         width={737}
         height={678}
         alt=""
